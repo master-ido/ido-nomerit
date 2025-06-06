b683f59f4dc33a36651c5d87ce060b2ab2accf1d
master-ido <idogrinwald@mail.tau.ac.il>
2025-06-06 16:41:27 +0300

    6.6

--- task_7/task_7.py
+++ task_7/task_7.py
@@ -23,12 +23,12 @@ ex, eh = 2, 0.2
 
 if __name__ == '__main__':
     forward_a, backward_a, centered_a = finite_diff_order_4_error(a, ax, ah)
-    print(f'(a) Analytic result: 4 forward: {forward_a}, backward:{backward_a}, centered:{centered_a}')
+    print(f'(a) Analytic result: 4, forward: {forward_a}, backward: {backward_a}, centered: {centered_a}')
     forward_b, backward_b, centered_b = finite_diff_order_4_error(b, bx, bh)
-    print(f'(b) Analytic result: 0.6745418604 forward: {forward_b}, backward:{backward_b}, centered:{centered_b}')
+    print(f'(b) Analytic result: 0.6745418604, forward: {forward_b}, backward: {backward_b}, centered: {centered_b}')
     forward_c, backward_c, centered_c = finite_diff_order_4_error(c, cx, ch)
-    print(f'(c) Analytic result: 1.141839607 forward: {forward_c}, backward:{backward_c}, centered:{centered_c}')
+    print(f'(c) Analytic result: 1.141839607, forward: {forward_c}, backward: {backward_c}, centered: {centered_c}')
     forward_d, backward_d, centered_d = finite_diff_order_4_error(d, dx, dh)
-    print(f'(d) Analytic result: -0.2600298981 forward: {forward_d}, backward:{backward_d}, centered:{centered_d}')
+    print(f'(d) Analytic result: -0.2600298981, forward: {forward_d}, backward: {backward_d}, centered: {centered_d}')
     forward_e, backward_e, centered_e = finite_diff_order_4_error(e, ex, eh)
-    print(f'(e) Analytic result: 8.389056099 forward: {forward_e}, backward:{backward_e}, centered:{centered_e}')
+    print(f'(e) Analytic result: 8.389056099, forward: {forward_e}, backward: {backward_e}, centered: {centered_e}')
